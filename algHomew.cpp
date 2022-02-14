7b8f9672c3f09840d634654512cada354b0ef352
speshik <speshik222@gmail.com>
2022-02-14 13:27:34 +0800

    f

---
+++ algHomew.cpp
@@ -0,0 +1,29 @@
+﻿// algHomew.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    ofstream out;
+    out.open("out.txt");
+    for (int i = 0; i < n; i++)
+    {
+        int arr[3] = { rand() % 6 + (-3), rand() % 6 + (-3), rand() % 6 + (-3) };
+        cout << arr[1] << endl;
+        for (int j = 0; j < 3; j++)
+        {
+            out << arr[j] << " ";
+        }
+        out << endl;
+    }
+
+
+    //cout << rand() % 6 + (-3) << endl;
+}
+
